b7c82a809a0dafa15d64685ff26ac9565960ba97
Beardificent <cis.magito@gmail.com>
2021-01-15 14:03:21 +0100

    initial commit after setting up most of the react routing to the spa

---
+++ src/main/java/com/example/reactspringbootworkshop/ReactSpringbootWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactspringbootworkshop/ReactSpringbootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
