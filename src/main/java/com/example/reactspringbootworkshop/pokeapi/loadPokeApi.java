3e6c65bd2bd42fde296a44be9cdb84a01bbaeb00
Beardificent <cis.magito@gmail.com>
2021-01-26 12:19:57 +0100

    managed to fetch the url from pokeApi, showing all data related to url given

--- src/main/java/com/example/reactspringbootworkshop/pokeapi/loadPokeApi.java
+++ src/main/java/com/example/reactspringbootworkshop/pokeapi/loadPokeApi.java
@@ -1,11 +1,17 @@
 package com.example.reactspringbootworkshop.pokeapi;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class loadPokeApi {
 
     public static void main(String[] args) {
+        BufferedReader bufferedReader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+
         try {
             URL url = new URL("https://pokeapi.co/api/v2/pokemon/");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -16,6 +22,22 @@ public class loadPokeApi {
             connection.setReadTimeout(5000);
             int statusCode = connection.getResponseCode();
             System.out.println(statusCode);
+
+            if (statusCode > 299){
+                bufferedReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while((line = bufferedReader.readLine()) != null){
+                    responseContent.append(line);
+                }
+                bufferedReader.close();
+            } else {
+                bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while((line = bufferedReader.readLine()) != null){
+                    responseContent.append(line);
+                }
+                bufferedReader.close();
+            }
+            System.out.println(responseContent.toString());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
